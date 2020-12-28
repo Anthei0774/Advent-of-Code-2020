21db075d83811a2c22eb6114cd1d31287d7b653a
Anthei0774 <76426427+Anthei0774@users.noreply.github.com>
2020-12-28 10:18:35 +0100

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,22 @@
+# Preprocesing
+
+input = open(main_folder + "5.txt").read()
+input = input.split("\n")
+for i in range(len(input)):
+    input[i] = input[i].replace("F", "0").replace("B", "1").replace("L", "0").replace("R", "1")
+    input[i] = [ int(input[i][:7], 2), int(input[i][7:], 2) ]
+    input[i] = input[i][0] * 8 + input[i][1]
+
+###################################################################################################
+
+# Part 1
+print("Max seat number:", max(input))
+
+###################################################################################################
+
+# Part 2
+
+for n in sorted(input):
+    if n + 1 not in input:
+        print("My seat number:", n + 1)
+        break
